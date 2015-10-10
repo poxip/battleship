39e8766d2fe2471d92be52171126598f22027fb5
Piotr <piotrantosz@protonmail.ch>
2015-10-10 13:45:21 +0200

    first commit

---
+++ battleship.py
@@ -0,0 +1,46 @@
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+count = 0
+
+while count <= 4:
+    print "You tried: %s of 5 times" % count
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+    guess_row -= 1
+    guess_col -= 1
+    count += 1
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        print_board(board)
+        break
+    elif guess_row >= 5 or guess_col >= 5:
+        print "Hey, that's not even on battle area!"
+    elif (board[guess_row][guess_col] == "X"):
+        print "You was shooting here already!"
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+        print_board(board)
+else:
+    print "Oops, game over."
