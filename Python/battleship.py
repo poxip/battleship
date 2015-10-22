a9a62bed474dc45cc229a477c8e3df5efbfce891
Piotr <piotrantosz@protonmail.ch>
2015-10-22 13:48:37 +0200

    Change messages & value error except

--- Python/battleship.py
+++ Python/battleship.py
@@ -23,34 +23,62 @@ def game():
     ship_col = random_col(board)
 
     count = 0
+    warning = "Hey, that's not the number. Game over."
+    again = "Do you want to play again? y/n\n"
+    congratulations = "Congratulations! You sunk my battleship!"
+    not_area = "Hey, that's not even on battle area!"
+    already_shot = "You was shooting here already!"
+    miss = "You missed my battleship!"
+    farewell = "Bye!"
 
     while count <= 4:
         print "You tried: %s of 5 times" % count
-        guess_row = int(raw_input("Guess Row:"))
+
+        guess_row = raw_input("Guess Row:")
+        try:
+            int(guess_row)
+        except ValueError:
+            print warning
+            break
+        else:
+            guess_row = int(guess_row)
+
         guess_col = int(raw_input("Guess Col:"))
+        try:
+            int(guess_col)
+        except ValueError:
+            print warning
+            break
+        else:
+            guess_col = int(guess_col)
+
         guess_row -= 1
         guess_col -= 1
         count += 1
         if guess_row == ship_row and guess_col == ship_col:
-            print "Congratulations! You sunk my battleship!"
+            print congratulations
             print_board(board)
-            break
+            new_game = raw_input(again)
+            if new_game == "yes" or new_game == "y":
+                game()
+            else:
+                print farewell
         elif guess_row >= 5 or guess_col >= 5:
-            print "Hey, that's not even on battle area!"
+            print not_area
         elif (board[guess_row][guess_col] == "X"):
-            print "You was shooting here already!"
+            print already_shot
         else:
-            print "You missed my battleship!"
+            print miss
             board[guess_row][guess_col] = "X"
             print_board(board)
     else:
         print "Game over! Ship was on row %s" %ship_row, "and col %s" % ship_col
         board[ship_row -1][ship_col -1] = "="
         print_board(board)
-        new_game = raw_input("Do you want to play again? y/n\n")
+        new_game = raw_input(again)
         if new_game == "yes" or new_game == "y":
             game()
         else:
-            print "Bye!"
+            print farewell
 
 game()
